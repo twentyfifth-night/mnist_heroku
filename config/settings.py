29c53fa9a85f5a4e7d6463e12860afb1ed3bca59
hara <sdukukjk@gmail.com>
2019-12-08 20:37:33 +0900

    path change

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/mnist/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
