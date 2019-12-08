859a1aed3218d260ddf7677e17d56464c11a458b
hara <sdukukjk@gmail.com>
2019-12-08 20:00:22 +0900

    path change3

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = 'mnist/static/'
-STATIC_ROOT = "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "mnist","static"))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
